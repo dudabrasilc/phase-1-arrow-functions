083d75fb60ffca1a1aef09c8983a6d7459cfb7d1
Maria Coitinho <brasildu@gmail.com>
2022-07-01 12:43:58 -0500

    commit tree 2f1a2a2d673ff9f4b141eea2a736cb7e261dbada

---
+++ index.js
@@ -0,0 +1,37 @@
+// function foo() {
+//     return 'bar';
+// }
+// console.log(foo());
+
+// const foo = function() {
+//     return 'bar';
+// }
+
+// console.log(foo());
+
+// const add = (parameter1, parameter2) => parameter1 + parameter2;
+// console.log(add(3,4));
+
+// const twoAdder = x => x + 2;
+// console.log(twoAdder(3));
+
+// const sum = (parameter1, parameter2) => {
+//     console.log(`Adding ${parameter1}`);
+//     console.log(`Adding ${parameter2}`);
+//     return parameter1 + parameter2;
+// }
+
+// const nums = [1,2,3];
+// const squares = nums.map(x => x ** 2);
+
+
+// finishedItems = overdueToDoItems.map(item => item.className = "complete");
+// header.innerText = `You finished ${finishedItems.lenght} items!`;
+
+const divide  = function() {
+    return 2000/100;
+}
+
+const square = parameter => parameter * parameter;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
